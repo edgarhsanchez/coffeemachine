148da4ace85c496bddde54e5d893fe6d46f7d636
Edgar H Sanchez <edgar0103sanchez@yahoo.com>
2019-06-23 17:06:26 -0500

    removed golang folders to be redone with golang modules after

--- CoffeeMachine/build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-echo "get go libs"
-go get -v ./...
-
-echo "building go coffeemachine application..."
-CGO_ENABLED=0 GOOS=linux go build -tags=jsoniter -a -installsuffix cgo -o main .
-echo "setting permission on file..."
-chmod +x ./main
