52029a04cb60c46522bd302aa1e6e800fda6b70e
Edgar Sanchez <edgar.sanchez@skookum.com>
2020-02-18 16:52:03 -0500

    added project for maker and added consul Microphone library

---
+++ CoffeeMachine/DotNet/CoffeeMachine/Controllers/MakerController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CoffeeMachine.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MakerController : ControllerBase
+    {
+        
+        private static Lazy<CoffeeMachine.Models.Order> _currentOrder = null;
+
+        public CoffeeMachine.Models.Order CurrentOrder {
+            get {
+                if(_currentOrder ==null) {
+                    _currentOrder = new Lazy<Models.Order>();
+                }
+
+                return _currentOrder.Value;
+            }
+        }
+        private readonly ILogger<MakerController> _logger;
+
+        public MakerController(ILogger<MakerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("api/IsBusy")]
+        public ActionResult<bool> IsBusy()
+        {
+            return CurrentOrder != null;
+        }
+
+
+
+    }
+}
---
+++ CoffeeMachine/DotNet/CoffeeMachine/models/Order.cs
@@ -0,0 +1,8 @@
+namespace CoffeeMachine.Models {
+     public class Order
+    {
+        public int Id { get; set; }
+
+        public string Coffee { get; set; }
+    }
+}
